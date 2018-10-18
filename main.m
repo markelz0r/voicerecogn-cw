413e6889c001d7a094c560f8a535dc1ab34315db
Andrew Odintsov <a1covker@gmail.com>
2018-10-18 12:14:00 +0100

    rando0m nois

--- main.m
+++ main.m
@@ -1,5 +1,10 @@
-y = audioread('audio_out_training\WAV - 2018-10-16 13-24-22.wav');
-x = y(:,1);
+y1 = audioread('audio_out_training\WAV - 2018-10-16 13-24-22.wav');
+x = y1(:,1);
 
 n = randn(2000,1);
-n2 = 
+n2 = n / 10^4;
+
+x(1:2000) = n2;
+
+
+audiowrite('training-2018-10-16 13-24-22.wav',y,Fs);
