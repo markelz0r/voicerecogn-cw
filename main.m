06f3e687e21f0020c40667df8a3e1383bab4e7a4
Andrew <a1covker@gmail.com>
2018-10-25 16:09:33 +0100

    initial hamming windowing and dff

--- main.m
+++ main.m
@@ -1,5 +1,30 @@
-y = audioread('audio_out_training\WAV - 2018-10-16 13-24-22.wav');
-x = y(:,1);
 
-n = randn(2000,1);
-n2 = 
+
+
+
+%reading audio file and resampling it
+fs_target = 16000;
+[audion_in,fs_old] = audioread('audio_out_training\WAV - 2018-10-16 13-24-22.wav');
+x = y(:,1); %taking chanel 1 only
+x_res = resample(x,fs_target,fs_old); %resampling onto 16khz
+
+
+%applying hamming window
+frame_length = 320;
+samples_num = length(x_res);
+frame_num = floor(samples_num/frame_length);
+for frame = 1:frame_num
+    sample1 = (frame * frame_length) - (frame_length - 1);
+    sample2 = (frame * frame_length);
+    ham = hamming(frame_length);
+    tf = x_res(sample1:sample2);
+    ham_res = ham.*tf;
+    dft = fft(ham_res);
+    plot(dft, 'r');
+end    
+
+
+
+
+
+
