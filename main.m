99f4d48b9ccc870dcd78861bfa9ed970a9902eaa
Andrew Odintsov <a1covker@gmail.com>
2018-11-01 13:03:18 +0000

    unfinished stuff on mfc generation

--- main.m
+++ main.m
@@ -57,6 +57,33 @@ end
 
 plot(vocalTractArr);
 
+
+
+vocalTractArr = vocalTractArr';
+numVectors = length(vocalTractArr);
+vectorPeriod = 
+numDims = 10;
+parmKind = 
+
+% Open file for writing:
+fid = fopen('randomname.mfc', 'w', 'ieee-be');
+
+% Write the header information% 
+fwrite(fid, numVectors, 'int32');    % number of vectors in file (4 byte int)
+fwrite(fid, vectorPeriod, 'int32');  % sample period in 100ns units (4 byte int)
+fwrite(fid, numDims * 4, 'int16');   % number of bytes per vector (2 byte int)
+fwrite(fid, parmKind, 'int16');      % code for the sample kind (2 byte int)
+
+% Write the data: one coefficient at a time:
+for i = 1: numVectors     
+	for j = 1:numDims      
+		fwrite(fid, vocalTractArr(i, j), 'float32');    
+	end
+end
+
+
+
+
 function [magSpec] = magAndPhase(shortTimeFrame)
 frame_length = 320;
     ham = hamming(frame_length);
@@ -65,6 +92,8 @@ frame_length = 320;
     magSpecFull = abs(dft);
     magSpec = magSpecFull(1:(length(magSpecFull)/2));  
 end
+
+
 
 
 
